7191995912a8df6c69a5b01b8e5f3482b8240f9e
Sean Lim <ponyo1188limboseng@gmail.com>
2016-11-11 08:33:27 +0800

    initial commit

---
+++ pastel./pastel./ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pastel.
+//
+//  Created by Sean Lim on 11/11/16.
+//  Copyright © 2016 tangentinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
